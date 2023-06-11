200d7489fc40525b07d6edd894078d9c608dc638
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-06-11 11:49:40 -0400

    completed lab

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+  const [itemName, setItemName] = useState("");
+  const [items, setItems] = useState([]);
+  const [itemCategory, setItemCategory] = useState("Produce");
+  
+  function handleNameChange (event) {
+    setItemName(event.target.value);
+  };
+
+  function handleCategoryChange (event) {
+    setItemCategory(event.target.value);
+  };
+
+  function handleItemFormSubmit(event) {
+    event.preventDefault();
+    
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+      
+    props.onItemFormSubmit(newItem)
+
+    setItemName("");
+    setItemCategory("Produce");
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleItemFormSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={itemName} onChange={handleNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={itemCategory} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
@@ -22,5 +49,4 @@ function ItemForm(props) {
     </form>
   );
 }
-
 export default ItemForm;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,34 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
+  function handleSearchChange(text) {
+    setSearchText(text);
+  }
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseSearchText = searchText.trim().toLowerCase();
+    if (selectedCategory !== "All" && item.category !== selectedCategory) {
+      return false;
+    }
 
-    return item.category === selectedCategory;
+    if (lowerCaseSearchText !== "" && !lowerCaseName.includes(lowerCaseSearchText)) {
+      return false;
+    }
+
+    return true;
+
   });
 
+  
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -28,5 +41,4 @@ function ShoppingList({ items }) {
     </div>
   );
 }
-
 export default ShoppingList;
